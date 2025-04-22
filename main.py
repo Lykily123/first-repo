c4364583863ea72c758af69596cf69018da531b2
Oleksandr Vishar <vishar.1998@gmail.com>
2025-04-22 15:08:48 +0300

    first program

--- main.py
+++ main.py
@@ -1,4 +1,104 @@
-name1 = 'Hello'
-name2 = 'world!'
+# перша програма/ Для кава-брейків на конференції закуплено круасани, стаканчики та пакунки кави. Ціна круасана — $1.04, ціна стаканчика — $0.34, ціна пакунка кави — $4.42. Потрібно скласти програму, яка обчислює, скільки повних доларів пішло на закупівлю їжі для кава-брейків і яка її повна вартість у центах.
 
-print(f'{name1} _ {name2}')
+price_croissant = 1.04
+price_cup = 0.34
+price_coffe = 4.42
+
+quantity_croissant = int(input('введіть кількість круасанів: '))
+quantity_cup = int(input('введіть кількість стаканчиків: '))
+quantity_coffe = int(input('введіть кількість кави: '))
+
+count_croisant = quantity_croissant * price_croissant
+count_cup = quantity_cup * price_cup
+count_coffe = quantity_coffe * price_coffe
+
+all_sum_dollar = count_croisant + count_cup + count_coffe
+
+all_sum_cent = count_croisant + count_cup + count_coffe * 100
+
+print(f'сума круасанів: {round(count_croisant, 2)}$')
+print(f'сума cтаканчиків: {round(count_cup, 2)}$')
+print(f'сума кохфе: {round(count_coffe, 2)}$')
+print(f'загальна сума: {all_sum_dollar}$ або {all_sum_cent}¢')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
